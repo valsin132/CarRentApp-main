d7cdcbfb77b38f896c99570d93970c70b8f4db7a
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2025-02-05 18:12:39 +0200

    changed footer

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import reservationsRoutes from "./routes/reservations.js";
 import { connectToDb, PORT } from "./db.js";
 import { fileURLToPath } from 'url';
 import path from "path";
-import mongoose from "mongoose";
 
 dotenv.config();
 
